cfbc4b3b20b0e5ffc693065c48f1ac631be58717
Chantal Wallace <theteatreehouse@gmail.com>
2018-07-13 00:27:46 +0000

    Done.

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -29,7 +29,7 @@ def adding
                   "oil paintings" => 3
                  }
   shipping_manifest["muskets"] = 2 
-  shipping_manifest["gunpowder"] = 4
+  shipping_manifest["gun powder"] = 4
 
   shipping_manifest
 end
